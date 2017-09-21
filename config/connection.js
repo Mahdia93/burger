0caca9761110d8f018b5170711a631e1ca96b734
Mahdia Hashimy <mahdia.hashimy@gmail.com>
2017-09-21 11:06:26 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ if(process.env.JAWSDB_URL){
   else{
   var connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Darkgrey",
+  host: "xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "wl32qlo1w49ax8xu",
+  password: "xp1oqpemd9a5gp9a",
   database: "burgers_db"
     });
   }
